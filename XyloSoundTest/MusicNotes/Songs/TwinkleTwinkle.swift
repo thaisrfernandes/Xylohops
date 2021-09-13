be776c9648efac4ec00b589d78828335d69f44ef
gabizorzo <gabriela.zorzo@edu.pucrs.br>
2021-09-13 14:39:08 -0300

    Twinkle Twinkle little star

---
+++ XyloSoundTest/MusicNotes/Songs/TwinkleTwinkle.swift
@@ -0,0 +1,13 @@
+//
+//  TwinkleTwinkle.swift
+//  XyloSoundTest
+//
+//  Created by Gabriela Zorzo on 13/09/21.
+//
+
+import Foundation
+
+class TwinkleTwinkle {
+    private (set) var song: [Note] = [.C,.C,.G,.G,.A,.A,.G,.F,.F,.E,.E,.D,.D,.C,.G,.G,.F,.F,.E,.E,.D,.G,.G,.F,.F,.E,.E,.D,.C,.C,.G,.G,.A,.A,.G,.F,.F,.E,.E,.D,.D,.C]
+    init(){}
+}
